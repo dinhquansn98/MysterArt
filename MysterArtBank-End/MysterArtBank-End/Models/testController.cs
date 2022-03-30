16ac7a73c56962429391a6eb1ec4e066509420a4
DESKTOP-BQVO7AF\My Admin <dinhquansn@gmail.com>
2022-03-30 09:43:49 +0700

    check commit

---
+++ MysterArtBank-End/MysterArtBank-End/Models/testController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MysterArtBank_End.Models
+{
+    public class testController : Controller
+    {
+        // GET: test
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
